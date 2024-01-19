3fe2266d8c63f9ad75e653b29272313e25a5e497
Martin Schneider <116349696+Martini2316@users.noreply.github.com>
2024-01-19 18:11:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,437 @@
+const wynikElement = document.getElementById('wynik-element');
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+let muzyka = document.getElementById("muzyka");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+    constructor(){
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        this.rotation = 0;
+        this.opacity = 1;
+        const image = new Image()
+        image.src = './img/ship.png';
+        image.onload = () => {
+            this.image = image; 
+            this.width = 100;
+            this.height = 75;
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height - this.height - 20
+            }
+        }
+        
+    }
+    draw() {
+        c.save()
+        c.globalAlpha = this.opacity;
+        c.translate(player.position.x + player.width/2, player.position.y + player.height/2)
+
+        c.rotate(this.rotation)
+        c.translate(-player.position.x - player.width/2, -player.position.y - player.height/2)
+
+        c.drawImage(
+            this.image, 
+            this.position.x, 
+            this.position.y,             
+            this.width, 
+            this.height
+        )
+
+        c.restore();
+    }
+
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x;
+        }
+        
+    }
+}
+
+class Projectile{
+    constructor({position, velocity}) {
+        this.position = position;
+        this.velocity = velocity;
+
+        this.radius = 7;
+    }
+
+    draw(){
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = 'red';
+        c.fill();
+        c.closePath();
+    }
+    update(){
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
+
+class Particle{
+    constructor({position, velocity, radius, color}) {
+        this.position = position;
+        this.velocity = velocity;
+
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw(){
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath();
+    }
+    update(){
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
+
+class InvaderProjectile{
+    constructor({position, velocity}) {
+        this.position = position;
+        this.velocity = velocity;
+
+        this.width = 5;
+        this.height = 15;
+    }
+
+    draw(){
+        c.fillStyle = 'LawnGreen';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    }
+    update(){
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
+
+class Invader {
+    constructor({position}){
+        
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        const image = new Image()
+        image.src = './img/enemy.png';
+        image.onload = () => {
+            this.image = image; 
+            this.width = image.width;
+            this.height = image.height;
+            this.position = {
+                x: position.x,
+                y: position.y
+            }
+        }
+        
+    }
+    draw() { 
+
+        c.drawImage(
+            this.image, 
+            this.position.x, 
+            this.position.y,             
+            this.width, 
+            this.height
+        )
+    }
+
+    update({velocity}) {
+        if (this.image) {
+            this.draw()
+            this.position.x += velocity.x;
+            this.position.y += velocity.y;
+        }
+        
+    }
+
+    shoot(invaderProjectiles){
+        invaderProjectiles.push(new InvaderProjectile({
+            position: {
+                x: this.position.x + this.width /2,
+                y: this.position.y + this.height
+            },
+            velocity: {
+                x: 0,
+                y: 5
+            }
+        }))
+    }
+}
+
+class Grid {
+    constructor(){
+        this.position = {
+            x: 0,
+            y: 0
+        }
+
+        this.velocity = {
+            x: 5,
+            y: 0
+        }
+
+        this.invaders = [];
+
+        const rows = Math.floor(Math.random() * 5 + 2);
+        const columns = Math.floor(Math.random() * 15 + 5);
+
+        this.width = columns * 50;
+
+        for (let x = 0; x<columns; x++){
+            for (let y = 0; y<rows; y++){
+                this.invaders.push(new Invader({position: {
+                    x: x * 50,
+                    y: y * 35
+                }}))
+            }
+        }
+    }
+
+    update(){
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.velocity.y = 0;
+
+        if(this.position.x + this.width >= canvas.width || this.position.x <= 0){
+            this.velocity.x = -this.velocity.x;
+            this.velocity.y = 35;
+        }
+    }
+}
+
+const player = new Player();
+const projectiles = [];
+const grids = [];
+const invaderProjectiles = [];
+const particles = [];
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+}
+
+let frames = 0;
+let randomInterval = Math.floor((Math.random() * 500) + 500);
+let game = {
+    over: false,
+    active: true
+}
+
+let wynik = 0;
+
+for( let i = 0; i < 100; i++) {
+    particles.push(
+        new Particle({
+            position: {
+                x: Math.random() * canvas.width,
+                y: Math.random() * canvas.height
+            },
+            velocity:{
+                x: 0,
+                y: 1
+            },
+            radius: Math.random() * 5,
+            color: 'white'
+        })
+    )
+}
+
+function animate() {
+    if (!game.active) return
+    requestAnimationFrame(animate);
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    particles.forEach(particle => {
+        if(particle.position.y - particle.radius >= canvas.height){
+            particle.position.x = Math.random() * canvas.width;
+            particle.position.y = -particle.radius; 
+        }
+        particle.update();
+    })
+    invaderProjectiles.forEach((invaderProjectile, index) => {
+        if(invaderProjectile.position.y + invaderProjectile.height >= canvas.height) {
+            setTimeout(() =>{
+                invaderProjectiles.splice(index, 1);
+            }, 0) 
+        }else{
+            invaderProjectile.update();
+        }   
+          
+        //SMIERC GRACZA + MUZYKA!!
+        if(invaderProjectile.position.y + invaderProjectile.height >= player.position.y && invaderProjectile.position.x + invaderProjectile.width >= player.position.x && invaderProjectile.position.x <= player.position.x + player.width){
+            setTimeout(() => {
+                invaderProjectiles.splice(index, 1);
+                player.opacity = 0;
+                game.over = true;
+                muzyka.pause();
+                var audiosmierc = new Audio('audio/gameOverSound.wav');
+                audiosmierc.play();
+            }, 0)
+            setTimeout(() => {
+                game.active = false;
+            }, 2000)
+            
+        }
+    })
+    projectiles.forEach((projectile, index) =>{
+        if (projectile.position.y + projectile.radius <= 0){
+            setTimeout(() =>{
+                projectiles.splice(index, 1);
+            }, 0)            
+        } else{
+            projectile.update();
+        }        
+    })
+
+    grids.forEach((grid, gridIndex) => {
+        grid.update();
+        //resp pociskow przeciwnikow
+        if(frames % 100  === 0 && grid.invaders.length > 0){
+            grid.invaders[Math.floor(Math.random() * grid.invaders.length)].shoot(invaderProjectiles);
+        }
+
+        grid.invaders.forEach((invader, i) => {
+            invader.update({velocity: grid.velocity});
+
+            projectiles.forEach((projectile, j) => {
+                if (projectile.position.y - projectile.radius <= invader.position.y + invader.height && 
+                    projectile.position.x + projectile.radius >= invader.position.x && 
+                    projectile.position.x - projectile.radius <= invader.position.x + invader.width && 
+                    projectile.position.y + projectile.radius >= invader.position.y){
+                    setTimeout(() => {
+                        const invaderFound = grid.invaders.find((invader2) => invader2 === invader)
+
+                        const projectileFound = projectiles.find((projectile2) => projectile2 === projectile)
+
+                        //Usuwanie przeciwnika i pocisku po trafieniu w przeciwnika
+                        if(invaderFound && projectileFound) {
+                            var audiosmiercprzeciwnika = new Audio('audio/dzwiekSmierciPrzeciwnika.wav');
+                            audiosmiercprzeciwnika.play();
+                            wynik += 100;
+                            wynikElement.innerHTML = wynik;
+                            
+                            grid.invaders.splice(i, 1);
+                            projectiles.splice(j, 1);
+
+                            if(grid.invaders.length > 0){
+                                const firstInvader = grid.invaders[0];
+                                const lastInvader = grid.invaders[grid.invaders.length-1];
+                                grid.width = lastInvader.position.x - 
+                                firstInvader.position.x + 
+                                lastInvader.width;
+                                grid.position.x = firstInvader.position.x;
+                            }
+                            else{
+                                grids.splice(gridIndex, 1);
+                            }
+                        }
+                        
+                    }, 0)
+                }
+            })
+        })
+    })
+
+    if(keys.a.pressed && player.position.x >= 0){
+        player.velocity.x = -7;
+        player.rotation = -.15;
+    }
+    else if (keys.d.pressed && player.position.x+player.width <= canvas.width){
+        player.velocity.x = 7;
+        player.rotation = .15;
+    }
+    else{
+        player.velocity.x = 0;
+        player.rotation = 0;
+    }
+
+    //resp przeciwnikow
+    if(frames % randomInterval === 0 ){
+        grids.push(new Grid());
+        randomInterval = Math.floor((Math.random() * 500) + 500);
+        frames = 0;
+    } 
+
+
+    frames++
+}
+
+animate();
+
+let spacjaCheck = false;
+
+addEventListener('keydown', ({key}) => {
+
+    if(game.over) return
+
+    switch (key){
+        case 'a':   
+            muzyka.play();       
+            keys.a.pressed = true;
+            break;
+        case 'd':
+            keys.d.pressed = true;
+            break;
+        case ' ':
+            if(spacjaCheck == false){
+                var audiostrzal = new Audio('audio/dzwiekStrzalu.wav');
+            audiostrzal.play();
+            projectiles.push(new Projectile({
+                
+                position: {
+                    x: player.position.x + player.width/2,
+                    y: player.position.y
+                },
+                velocity: {
+                    x: 0,
+                    y: -10
+                }
+                }));
+                spacjaCheck = true;
+            }            
+            
+            break;
+    }
+})
+
+addEventListener('keyup', ({key}) => {
+    switch (key){
+        case 'a':          
+            keys.a.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
+            break;
+        case ' ':
+            spacjaCheck = false;
+            break;
+    }
+})
